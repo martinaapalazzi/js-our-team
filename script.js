35acc68fbf6acf19fdebde7fe264219e6ed774fd
martinaapalazzi <martinaapalazzi@gmail.com>
2023-12-18 22:42:14 +0100

    Coorected bonus 1

--- script.js
+++ script.js
@@ -15,59 +15,51 @@ const employers = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        //image: 'wayne-barnett-founder-ceo.jpg',
+        image: 'wayne-barnett-founder-ceo.jpg',
     },
 
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        //image: 'angela-caroll-chief-editor.jpg',
+        image: 'angela-caroll-chief-editor.jpg',
     },
 
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        //image: 'walter-gordon-office-manager.jpg',
+        image: 'walter-gordon-office-manager.jpg',
     },
 
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        //image: 'angela-lopez-social-media-manager.jpg',
+        image: 'angela-lopez-social-media-manager.jpg',
     },
 
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        //image: 'scott-estrada-developer.jpg',
+        image: 'scott-estrada-developer.jpg',
     },
     
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        //image: 'barbara-ramos-graphic-designer.jpg',
+        image: 'barbara-ramos-graphic-designer.jpg',
     },
 ];
 
 
-const imgList = [
-    'wayne-barnett-founder-ceo.jpg',
-    'angela-caroll-chief-editor.jpg',
-    'walter-gordon-office-manager.jpg',
-    'angela-lopez-social-media-manager.jpg',
-    'scott-estrada-developer.jpg',
-    'barbara-ramos-graphic-designer.jpg',
-];
-
-
-
 for (let i = 0; i < employers.length; i++) {
     console.log(employers[i]);
-    for (let key in employers[i]){
+    let myTeam = document.getElementById('my-employers');
+        myTeam.innerHTML += `<div class="img_container"><img src="${employers[i]['image']}"></div><div class="description"><h2>${employers[i]['name']}</h2><p>${employers[i]['role']}</p></div>`;
+
+    /*for (let key in employers[i]){
         console.log(employers[i][key]);
         let myTeam = document.getElementById('my-employers');
-        myTeam.innerHTML += `<div> ${employers[i][key]} </div> <div><img src="${imgList[i]}" alt=""></div>`;
+        myTeam.innerHTML += `<div class="img_container"><img src="${employers[i]['image']}"></div><div class="description"><h2>${employers[i]['name']}</h2><p>${employers[i]['role']}</p></div>`;
         console.log(myTeam);
-    }
+    }*/
 };
 
