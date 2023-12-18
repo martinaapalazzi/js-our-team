789ec5b80b97467d6d0ccc886ce2bbeb41ca40e1
martinaapalazzi <martinaapalazzi@gmail.com>
2023-12-18 16:22:28 +0100

    milestone 3

--- script.js
+++ script.js
@@ -12,37 +12,37 @@ Stampare le stesse informazioni su DOM sottoforma di stringhe.
 */
 
 const employers = [
-    employ1 = {
+    {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
         image: 'wayne-barnett-founder-ceo.jpg',
     },
 
-    employ2 = {
+    {
         name: 'Angela Caroll',
         role: 'Chief Editor',
         image: 'angela-caroll-chief-editor.jpg',
     },
 
-    employ3 = {
+    {
         name: 'Walter Gordon',
         role: 'Office Manager',
         image: 'walter-gordon-office-manager.jpg',
     },
 
-    employ4 = {
+    {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
         image: 'angela-lopez-social-media-manager.jpg',
     },
 
-    employ5 = {
+    {
         name: 'Scott Estrada',
         role: 'Developer',
         image: 'scott-estrada-developer.jpg',
     },
     
-    employ6 = {
+    {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
         image: 'barbara-ramos-graphic-designer.jpg',
@@ -51,3 +51,12 @@ const employers = [
 
 console.log('emplyers', employers, typeof employers);
 
+for (let i = 0; i < employers.length; i++) {
+    console.log(employers[i]);
+    for (let key in employers[i]){
+        console.log(employers[i][key]);
+        let myTeam = document.getElementById('my-employers');
+        myTeam.innerHTML += employers[i][key];
+        console.log(myTeam);
+    }
+}
