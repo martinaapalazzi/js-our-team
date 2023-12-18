edc1fdbef2de4f488cb71754804b06766c1366af
martinaapalazzi <martinaapalazzi@gmail.com>
2023-12-18 15:25:54 +0100

    milestone 1 & 2

---
+++ script.js
@@ -0,0 +1,53 @@
+/*
+
+MILESTONE 0:
+Creare l'array di oggetti con le informazioni fornite.
+
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto.
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe.
+
+*/
+
+const employers = [
+    employ1 = {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+
+    employ2 = {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    },
+
+    employ3 = {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+
+    employ4 = {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    },
+
+    employ5 = {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    },
+    
+    employ6 = {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+console.log('emplyers', employers, typeof employers);
+
